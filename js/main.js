39f4a1e265f969d35d9e706e0e3d79e8a5fcd406
Lucas Freitas <lucas.freitas@sigcertificadora.com.br>
2019-10-03 10:46:56 -0300

    computed, watch e  lifecycle hooks

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 var app = new Vue({
     el: "#app",
+    beforeCreate() {
+        console.log('beforeCreate');
+        console.log('status', this.status);
+        console.log('elemento', this.$el);
+    },
+    created() {
+        console.log('Created');
+        console.log('status', this.status);
+        console.log('elemento', this.$el);
+    },
+    beforeMount() {
+        console.log('beforeMount');
+        console.log('elemento', this.$el);
+    },
+    mounted() {
+        console.log('mounted');
+        console.log('elemento', this.$el);
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate');
+    },
+    updated() {
+        console.log('updated');
+    },
     data: {
         status: false,
         titulo_true: "O status é verdadeiro!",
@@ -13,6 +37,7 @@ var app = new Vue({
         texto: " ",
         logo: "imgs/logo.svg",
         alt: "logo da empresa",
+        lifecycle: "imgs/lifecycle.png",
         color: "",
         limpar: ['btn-sm', 'btn-danger'],
         enviar: {
@@ -20,6 +45,8 @@ var app = new Vue({
             'btn-primary': true,
 
         },
+        firstName: "",
+        lastName: "",
     },
     filters: {
         toUpCase(str) {
@@ -29,6 +56,28 @@ var app = new Vue({
     methods: {
         teste() {
             alert('Enviado!');
+        },
+        updateUser() {
+            alert('Usuario atualizado com sucesso');
         }
+
+    },
+    computed: {
+        fullName() {
+
+            return this.firstName + ' ' + this.lastName;
+        }
+    },
+    watch: {
+        firstName(newValue, oldValue) {
+            console.log('oldValue:', oldValue);
+            console.log('newValue', newValue);
+            this.updateUser();
+        },
+        lastName(newValue, oldValue) {
+            console.log('oldValue:', oldValue);
+            console.log('newValue', newValue);
+            this.updateUser();
+        },
     }
 })
